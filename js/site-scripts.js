f4aab0e68e02d73d210a1e127a5199430f2c09ad
Jason Britton <velograph@gmail.com>
2018-09-19 16:25:37 -0700

    More desktop header cleanup and mobile menu added.

--- js/site-scripts.js
+++ js/site-scripts.js
@@ -1,5 +1,22 @@
 jQuery(document).ready(function(){
 
-	
+	if (jQuery(window).width() < 860) {
+
+		// Mobile menu toggle
+		jQuery(".slide-out").hide();
+		jQuery(".menu-hook").click(function(){
+			jQuery(".slide-out").slideToggle(700);
+		});
+	}
+
+	// Sticky header if the logo isn't the circle
+	// if (jQuery(window).width() > 860) {
+	//
+	// 	// Desktop sticky header
+	// 	jQuery(".desktop-header").sticky({
+	// 		topSpacing: 0
+	// 	});
+	// }
+
 
 });
