b332fb1f0ddc793c978e4e39b9b55d813b13f9bb
Jason Britton <velograph@gmail.com>
2018-09-27 14:36:58 -0700

    More header/footer work

--- js/site-scripts.js
+++ js/site-scripts.js
@@ -3,10 +3,10 @@ jQuery(document).ready(function(){
 	if (jQuery(window).width() < 860) {
 
 		// Mobile menu toggle
-		// jQuery(".slide-out").hide();
-		// jQuery(".menu-hook").click(function(){
-		// 	jQuery(".slide-out").slideToggle(700);
-		// });
+		jQuery(".slide-out").hide();
+		jQuery(".menu-hook").click(function(){
+			jQuery(".slide-out").slideToggle(700);
+		});
 	}
 
 	// Sticky header if the logo isn't the circle
